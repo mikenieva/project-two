39f8bd737a3e32dbad608eb38f997d4fcd04cdd2
Mike Nieva <miguelnieva@conexionew.com>
2018-08-09 21:38:20 -0500

    Bye favicon

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
@@ -52,7 +51,6 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 const authRoutes = require ('./routes/auth-routes');
